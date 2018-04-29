d76ea0444b10d9c72c01ff8d4928f58032d8194d
Matthew Jeffreys <mdj20@pitt.edu>
2018-04-28 23:36:17 -0400

    GameInfoPanel work.

--- ScrumChessSwing/src/com/mdj20/scrumchessswing/ui/components/GameInfoPanel.java
+++ ScrumChessSwing/src/com/mdj20/scrumchessswing/ui/components/GameInfoPanel.java
@@ -1,7 +1,27 @@
 package com.mdj20.scrumchessswing.ui.components;
 
+import java.awt.GridLayout;
+
 import javax.swing.JPanel;
 
-public class GameInfoPanel extends JPanel {
+import com.mdj20.scrumchessswing.ui.CentralUIAccess;
 
+public class GameInfoPanel extends JPanel {
+	
+	CentralUIAccess centralUIAccess;
+	GameStatusField gameStatusFeild;
+	GameTypeSpinner gameTypeSpinner;
+	TurnTextField turnTextField;
+	GameIdInputBox gameIdInputBox;
+	
+	GameInfoPanel(CentralUIAccess centralUIAccess){
+		this.centralUIAccess = centralUIAccess;
+		GridLayout gridLayout = new GridLayout(4,1);
+		this.setLayout(gridLayout);
+		//gameIdInputBox = new GameId;
+	}
+
+	
+	
+	
 }
